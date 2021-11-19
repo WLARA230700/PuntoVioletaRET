c5aeaef5f4a3e02329c6a8b972e52cac3022510f
WLARA230700 <willy.lara@ucr.ac.cr>
2021-11-18 19:23:26 -0600

    Copy Temas de interés

--- resources/views/temas_interes.blade.php
+++ resources/views/temas_interes.blade.php
@@ -7,22 +7,6 @@
             <div class="row temas-interes-header">
                 <div class="row">
                     <h1>Temas de Interés</h1>
-                    <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in, cu justo
-                        suscipiantur mel. Mel facilis sensibus petentium in, ne ignota mollis suavitate mei, modus reque
-                        signiferumque in cum. Ut molestiae complectitur eam, partiendo necessitatibus ad vix. Putent
-                        semper nusquam ...</p>
-                </div>
-                <div class="row select-box">
-                    <div id="txt-select-box">
-                        <p>Violencia doméstica</p><img src="../imgs/svg/icon_dropdown.svg"
-                            alt="icon_dropdown">
-                    </div>
-                    <div class="options-container">
-                        <div class="col option"><a href="#" class="btn-repo">Violencia <br> doméstica</a></div>
-                        <div class="col option"><a href="#" class="btn-repo">Voliencia <br> sexual</a></div>
-                        <div class="col option"><a href="#" class="btn-repo">Relaciones <br> impropias</a></div>
-                        <div class="col option"><a href="#" class="btn-repo">Acoso <br> callejero</a></div>
-                    </div>
                 </div>
             </div>
 
@@ -32,45 +16,55 @@
                         <h2>Tipos de violencia</h2>
 
                         <div>
-                            <h3>Violencia autoinflingida</h3>
-                            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in,
-                                cu justo suscipiantur mel. Mel facilis sensibus petentium in, ne ignota mollis suavitate
-                                mei, modus reque signiferumque in cum. Ut molestiae complectitur eam, partiendo
-                                necessitatibus ad vix. Putent semper nusquam ...</p>
+                            <h3>Violencia doméstica</h3>
+                            <p>Este tipo de violencia implica las situaciones de maltrato físico, psicológico, emocional, sexual, patrimonial o económico, que son  ejercidas por una persona que ha tenido o tiene relación o parentesco con la persona agredida.</p>
+                            <h3>¿Quienes pueden sufrir violencia doméstica?</h3>
+                            <p>Cualquier persona puede sufrir violencia doméstica, tanto mujeres, personas con discapacidad, niños (as), adultos mayores, entre otros.</p>
                             <hr>
 
-                            <h3>Violencia interpersonal</h3>
-                            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in,
-                                cu justo suscipiantur mel. Mel facilis sensibus petentium in, ne ignota mollis suavitate
-                                mei, modus reque signiferumque in cum. Ut molestiae complectitur eam, partiendo
-                                necessitatibus ad vix. Putent semper nusquam ...</p>
+                            <h3>Violencia sexual</h3>
+                            <p>Este tipo de violencia se hace presente cuando no se le permite a la mujer tener control sobre su sexualidad, y por ende se le amenaza e intimida de manera que la persona se sienta culpable y acceda a lo que se le está imponiendo. En ocasiones se hace uso de la fuerza física o de armas para ello. La mayoría de víctimas de violencia sexual son mujeres, siendo los hombres en su mayoría (más del 90%) los agresores, además se presentan casos de niños abusados y chicas transgénero. </p>
+                            <h3>¿Cómo se puede presentar?</h3>
+                            <p>- Incesto. <br>
+                            - Abuso sexual.<br>
+                            - Manoseos.<br>
+                            - Violación con o sin penetración.<br>
+                            - Acoso sexual.<br>
+                            - Prostitución forzada.<br>
+                            - Trata de personas.<br>
+                            - Obligar a masturbar o a tocar a otra persona.
+                            </p>
                             <hr>
 
-                            <h3>Violencia física</h3>
-                            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in,
-                                cu justo suscipiantur mel. Mel facilis sensibus petentium in, ne ignota mollis suavitate
-                                mei, modus reque signiferumque in cum. Ut molestiae complectitur eam, partiendo
-                                necessitatibus ad vix. Putent semper nusquam ...</p>
+                            <h3>Violencia de género</h3>
+                            <p>Refiere a aquellos actos que ejercen algún tipo de daño, ya sea físico, verbal o psicológico, a una persona basándose en su género, orientación sexual o identidad sexual.</p>
+                            <h3>¿Cuándo se está ejerciendo violencia de género?</h3>
+                            <p>Por ejemplo cuando se trata de minimizar a la mujer diciendo “tenía que ser mujer” o cualquier otro comentario similar que pretenda inferiorizarle.</p>
+                            <p>Tanto hombres como mujeres reproducen la violencia de género, la cual ha sido infundada como un llamado “mandato cultural” el cual es aprendido desde la infancia y en definitiva se debe erradicar.</p>
                             <hr>
 
-                            <h3>Violencia verbal</h3>
-                            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in,
-                                cu justo suscipiantur mel. Mel facilis sensibus petentium in, ne ignota mollis suavitate
-                                mei, modus reque signiferumque in cum. Ut molestiae complectitur eam, partiendo
-                                necessitatibus ad vix. Putent semper nusquam ...</p>
+                            <h3>Acoso callejero</h3>
+                            <p>El Protocolo de intervención policial en la atención de casos de acoso sexual en espacios públicos o de acceso público (2018) del Ministerio de Seguridad Pública define acoso callejero como:</p>
+                            <p>“cualquier conducta con connotación sexual explícita o implícita, proviene de un desconocido, ocurrido en espacios públicos y tiene el potencial de provocar malestar en la persona acosada” p.4</p>
+                            <p>También es importante mencionar que “El acoso sexual lesiona la integridad física, psicológica y sexual de las víctimas que se sienten invadidas por las palabras, gestos, tocamiento e incluso intentos de violación o abuso sexual”. (INAMU)</p>
+                            <h3>¿Cuáles son las sanciones para quienes cometen acoso callejero?</h3>
+                            <p>A quienes comentan acoso sexual callejero se les podrá imponer penas que van desde 6 meses hasta 1 año y medio de prisión, o bien al pago de 15 hasta 45 días multa.</p>
                             <hr>
 
-                            <h3>Violencia psicológica</h3>
-                            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in,
-                                cu justo suscipiantur mel. Mel facilis sensibus petentium in, ne ignota mollis suavitate
-                                mei, modus reque signiferumque in cum. Ut molestiae complectitur eam, partiendo
-                                necessitatibus ad vix. Putent semper nusquam ...</p>
+                            <h3>Relaciones impropias</h3>
+                            <p>Es aquella relación existente entre una persona mayor de edad y un niño, niña o adolescente menor de 18 años. En este tipo de relaciones se da una desigualdad de poder entre ambas partes siendo la persona menor de edad quien lleva la desventaja, lo cual puede desencadenar en situaciones de violencia. La persona mayor cuenta con más experiencias de vida, más conocimiento y en algunos casos con mayor capacidad económica, por lo que se ejerce un control sobre la persona menor, este control puede manifestarse como violencia emocional, psicológica, patrimonial o física, poniendo en riesgo la vida de la persona menor.</p>
+                            <h3>¿Cómo saber si una relación es impropia?</h3>
+                            <p>- Si el o la adolescente se encuentra entre los 13 y 15 años y mantiene una relación con una persona 5 años mayor.</p>
+                            <p>- Si el o la adolescente se encuentra entre los 15 y 18 años y mantiene una relación con una persona 7 años mayor.</p>
+                            <p>- Si el o la adolescente se encuentra entre los 15 y 18 años y mantiene una relación con una persona que supera más de los 7 años de diferencia.</p>
+                            <h3>¿Qué consecuencias pueden obtenerse a raíz de una relación impropia?</h3>
+                            <p>- La persona menor de edad podría abandonar o retrasar sus estudios.</p>
+                            <p>- Puede ocasionar un sometimiento tanto emocional como económico, orientado a los deseos de la persona mayor.</p>
+                            <p>- Generar un daño a la autoestima de la persona menor.</p>
+                            <p>- La persona menor de edad se aísla de sus padres, amigos o familiares y la relación que mantenía con ellos.</p>
+                            <p>- Aparecen sentimientos negativos como tristeza, depresión.</p>
                         </div>
 
-                    </div>
-                    <div>
-                        <h2>Infografía con la Ley</h2>
-                        <img src="../imgs/ejemplo_infografia.png" alt="Infografía">
                     </div>
                 </div>
                 <div class="col-sm-6">
@@ -78,51 +72,33 @@
                         <h2>Señales de alarma</h2>
 
                         <div>
-                            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in,
-                                cu
-                                justo suscipiantur mel.</p>
-
-                            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in,
-                                cu
-                                justo suscipiantur mel.</p>
-
-                            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in,
-                                cu
-                                justo suscipiantur mel.</p>
-
-                            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in,
-                                cu
-                                justo suscipiantur mel.</p>
-
-                            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in,
-                                cu
-                                justo suscipiantur mel.</p>
+                            <p>- Bromas hirientes o humillantes.</p>
+                            <p>- Insultos o humillaciones.</p>
+                            <p>- Control en la forma de vestirse o maquillarse.</p>
+                            <p>- Control sobre las finanzas.</p>
+                            <p>- Control sobre las amistades y las relaciones familiares.</p>
+                            <p>- Violencia física como golpes, pellizcos, empujones o cachetadas.</p>
+                            <p>- Relaciones sexuales forzadas o sin consentimiento.</p>
+                            <p>- Amenazas con armas u objetos peligrosos.</p>
                         </div>
 
                     </div>
                     <div>
                         <h2>¿Cómo y dónde denunciar?</h2>
-
                         <div>
+                            <p>En caso de que usted o alguna persona que conozca esté pasando por una situación de violencia puede denunciar por los siguientes medios:</p>
                             <h3>¿Cómo denunciar?</h3>
-                            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in,
-                                cu
-                                justo suscipiantur mel. Mel facilis sensibus petentium in, ne ignota mollis suavitate
-                                mei,
-                                modus reque signiferumque in cum. Ut molestiae complectitur eam, partiendo
-                                necessitatibus ad
-                                vix. Putent semper nusquam ...</p>
+                            <p>Llame al 9-1-1: Este es un Sistema de Emergencias al cual se puede acceder de manera confidencial y gratuita. A través de esta línea es posible ponerse en contacto con el Centro operativo de atención a la violencia intrafamiliar (COAVIF) quien se encarga de atender situaciones de mujeres menores de edad, mujeres mayores de edad y personas adultas mayores víctimas de violencia de diversa índole (sexual, intrafamiliar,  de pareja, etc), víctimas de explotación sexual, entre otros casos.</p>
 
                             <hr>
 
                             <h3>Dónde denunciar?</h3>
-                            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in,
-                                cu
-                                justo suscipiantur mel. Mel facilis sensibus petentium in, ne ignota mollis suavitate
-                                mei,
-                                modus reque signiferumque in cum. Ut molestiae complectitur eam, partiendo
-                                necessitatibus ad
-                                vix. Putent semper nusquam ...</p>
+                            <h3>INAMU:</h3>
+                            <p>Delegación de la Mujer→ Se encarga de brindar atención legal, social y psicológica a mujeres que han sido víctimas de violencia. Tel.  2527-3761.</p>
+                            <p>Departamento de Condición Jurídica y Protección de los Derechos Humanos de las Mujeres → Centro de Información y Orientación. Tel. 2527-1911. Correo electrónico cio@inamu.go.cr</p>
+
+                            <h3>Ministerio Público:</h3>
+                            <p>Puede presentarse a la Fiscalía más cercana y presentar su denuncia, la cual puede ser verbal o escrita.</p>
                         </div>
 
                     </div>
@@ -130,24 +106,17 @@
                         <h2>Requisitos para presentar una denuncia</h2>
 
                         <div>
-                            <h3>1 - Scelerisque in dictum non consectetur</h3>
-                            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in,
-                                cu
-                                justo suscipiantur mel.</p>
-                            <hr>
-
-                            <h3>2 - Scelerisque in dictum non consectetur</h3>
-                            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in,
-                                cu
-                                justo suscipiantur mel.</p>
-                            <hr>
-
-                            <h3>3 - Scelerisque in dictum non consectetur</h3>
-                            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in,
-                                cu
-                                justo suscipiantur mel.</p>
+                            <h3>Ministerio Público</h3>
+                            <p>1. Documento de identidad en buen estado y vigente (cédula de identidad, DIMEX o pasaporte).</p>
+                            <p>2. Nombre  de la persona denunciada y de ser posible el número de cédula si se tiene.</p>
+                            <p>3. Lugar para ubicar a la persona.</p>
+                            <p>4. En caso de contar con testigos y/o pruebas  de lo ocurrido estas deben ser presentadas.</p>
                         </div>
 
+                    </div>
+                    <div>
+                        <h2>Infografía con la Ley</h2>
+                        <img src="../imgs/ejemplo_infografia.png" alt="Infografía">
                     </div>
                 </div>
             </div>
